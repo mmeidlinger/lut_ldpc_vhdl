5e7199c24ba1392fc142166cb7992ef0ff215aa2
Reza Ghanaatian <reza.ghanaatian@epfl.ch>
2018-04-07 23:44:08 +0200

    extra VHDL files removed from repo, README updated for VHDL simulation

--- ModelSim/BIN_Decoder/run.sh
+++ ModelSim/BIN_Decoder/run.sh
@@ -3,5 +3,4 @@
 # Run the testbench
 eda mgc vsim -novopt -t 1ps \
 			-do BIN_Decoder/SimScripts.do \
-                        -wlf /usr/scratch/tclrhel13/ghanaati/waveFolder/vsim_gate.wlf \
                         work.TopLevelDecoderTB &
